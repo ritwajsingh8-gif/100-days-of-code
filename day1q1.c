48aa509df2654603160a078d688a7174fa449ebc
ritwajsingh8-gif <ritwajsingh8@gmail.com>
2026-08-16 16:38:54 +0530

    day1 q1

---
+++ day1q1.c
@@ -0,0 +1,9 @@
+// write a program to input two numbers abd display their sum.
+#include <stdio.h>
+int main() {
+int a,b,sum;
+scanf("%d %d",&a,&b);
+sum = a+b;
+printf("Sum = %d",sum);
+return 0;
+}
